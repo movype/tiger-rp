dd751335168ee0a7c277301fcd8e8817d8339e51
movype <98736903+movype@users.noreply.github.com>
2022-02-27 11:44:13 +0800

    Update trackDownload.js

--- assets/js/trackDownload.js
+++ assets/js/trackDownload.js
@@ -8,7 +8,7 @@ if(window.location.search!==''){window.location.href='/'}}
 if(qs['utm_source']!='youtube'&&(qs['source']=='vk'||qs['source']=='d'||qs['source']=='d1'||qs['source']=='a'||qs['source']=='a1'||qs['source']=='yt')){Cookies.set('bundleVersion',qs['source'],{'path':'/'})}
 if(qs['utm_source']=='youtube'){Cookies.set('bundleVersion','yt',{'path':'/'})}
 trackDownload=()=>{reachGoal('launcher_download')
-const bundle=Cookies.get('bundleVersion')||null;const suffix=bundle?`-${bundle.replace(/\d/g,'')}`:'';window.location.href=`rr-launcher${suffix}.apk`}
+const bundle=Cookies.get('bundleVersion')||null;const suffix=bundle?`-${bundle.replace(/\d/g,'')}`:'';window.location.href=`Уппс, лаунчер еще не готов!${suffix}.apk`}
 reachGoal=(goal)=>{let reachedGoals=JSON.parse(window.localStorage.getItem('reachedGoals'))||{}
 if(reachedGoals[goal]!==true){if(window.dataLayer){const tagManagerArgs={dataLayer:{category:'event',event:goal,action:goal},}
 window.dataLayer.push(tagManagerArgs.dataLayer)}
